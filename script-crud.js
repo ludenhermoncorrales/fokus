7f995c1ef7316a2bd3e41964d807452c99b09277
ludenhermoncorrales <ludencorrales@gmal.com>
2025-11-29 10:40:27 -0300

    the -Cancel Task- button clears and hides the task completion form

--- script-crud.js
+++ script-crud.js
@@ -1,60 +1,86 @@
 const btnAddTarefa = document.querySelector('.app__button--add-task');
-const formAddTarefa= document.querySelector('.app__form-add-task')
+const btnCancelTask = document.querySelector('.app__form-footer__button--cancel')
+const formAddTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
 const ultask = document.querySelector('.app__section-task-list')
 
+
+
+
 const listTask = JSON.parse(localStorage.getItem('fullTarefas')) || [];
 
-function tuUpdateTask () {
+function tuUpdateTask() {
     localStorage.setItem('fullTarefas', JSON.stringify(listTask))
 }
 
 // Creat task 
 function creatElementTask(tarefa) {
-    const li= document.createElement('li')
+    const li = document.createElement('li')
     li.classList.add('app__section-task-list-item')
 
-    const svg= document.createElement('svg')
-    svg.innerHTML=`<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    const svg = document.createElement('svg')
+    svg.innerHTML = `<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>`
 
-        const paragraph= document.createElement('p') 
-        paragraph.textContent = tarefa.descricao
-        paragraph.classList.add('app__section-task-list-item-description')
+    const paragraph = document.createElement('p')
+    paragraph.textContent = tarefa.descricao
+    paragraph.classList.add('app__section-task-list-item-description')
 
-        const button= document.createElement('button')
-        button.textContent = 'EDITAR'
-        button.classList.add('app_button-edit')
+    const button = document.createElement('button')
+    button.textContent = 'EDITAR'
+    button.classList.add('app_button-edit')
 
-        const imgButton= document.createElement('img')
-        imgButton.setAttribute('src', 'imagens/edit.png')
+    const imgButton = document.createElement('img')
+    imgButton.setAttribute('src', 'imagens/edit.png')
 
-        button.onclick = () =>{
-            const newDescription = prompt('Qual a nova da tarefa?');
-            console.log('New description task -->', newDescription )
-            if (newDescription) {
-                // Altera a camada visual da tela
-                paragraph.textContent = newDescription;
-                // salva na localStorage (application) a nova descrição
-                tarefa.descricao = newDescription;
-                tuUpdateTask();
-            }
+    button.onclick = () => {
+        const newDescription = prompt('Qual a nova da tarefa?');
+        console.log('New description task -->', newDescription)
+        if (newDescription) {
+            // Altera a camada visual da tela
+            paragraph.textContent = newDescription;
+            // salva na localStorage (application) a nova descrição
+            tarefa.descricao = newDescription;
+            tuUpdateTask();
         }
+    }
 
-        button.append(imgButton)
+    button.append(imgButton)
 
-        li.append(svg)
-        li.append(paragraph)
-        li.append(button)
+    li.append(svg)
+    li.append(paragraph)
+    li.append(button)
 
-        return li
+    return li
 }
 
 btnAddTarefa.addEventListener('click', () => {
     formAddTarefa.classList.toggle('hidden')
 })
+
+// ------------------------------------------------
+
+// Leiura do click do botão cancelar e limpar o campo textArea 
+
+// btnCancelTask.addEventListener('click', () => {
+// textArea.value = ''
+// formAddTarefa.classList.add('hidden')
+// })
+
+const clearTextArea = () => {
+    textArea.value = '';
+    formAddTarefa.classList.add('hidden');
+}
+
+btnCancelTask.addEventListener('click', clearTextArea)
+
+// ------------------------------------------------
+
+
+
+
 
 formAddTarefa.addEventListener('submit', (evento) => {
     // previne o comportamento padrão da pagina preventDefault();
@@ -75,6 +101,6 @@ formAddTarefa.addEventListener('submit', (evento) => {
 })
 
 listTask.forEach(task => {
-    const elementTask= creatElementTask(task)
+    const elementTask = creatElementTask(task)
     ultask.append(elementTask)
 });
